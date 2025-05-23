b68a36fe812311339c3851c666b088d457e0460f
Bikram Kumar <bikram.kumar@harness.io>
2025-05-23 18:49:47 +0530

    added

--- db/example-changelog.sql
+++ db/example-changelog.sql
@@ -3,54 +3,54 @@
     -- testing
 --changeset your.name:1 labels:example-label context:example-context
 --comment: example comment
-CREATE TABLE person (
+CREATE TABLE person1 (
     id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
     name VARCHAR(50) NOT NULL,
     address1 VARCHAR(50),
     address2 VARCHAR(50),
     city VARCHAR(30)
 );
---rollback DROP TABLE person;
+--rollback DROP TABLE person1;
 
 --changeset your.name:2 labels:example-label context:example-context
 --comment: example comment
-CREATE TABLE company (
+CREATE TABLE company1 (
     id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
     name VARCHAR(50) NOT NULL,
     address1 VARCHAR(50),
     address2 VARCHAR(50),
     city VARCHAR(30)
 );
---rollback DROP TABLE company;
+--rollback DROP TABLE company1;
 
 --changeset other.dev:3 labels:example-label context:example-context
 --comment: example comment
-ALTER TABLE person
+ALTER TABLE person1
 ADD [country] varchar(100)
---rollback ALTER TABLE person DROP COLUMN country;
+--rollback ALTER TABLE person1 DROP COLUMN country;
 
 
 --changeset other.dev:4 labels:example-label context:example-context
 --comment: example comment
-ALTER TABLE person
+ALTER TABLE person1
 ADD [state] varchar(10)
---rollback ALTER TABLE person DROP COLUMN state;
+--rollback ALTER TABLE person1 DROP COLUMN state;
 
 
 --changeset other.dev:5 labels:example-label context:example-context
 --comment: example comment
-ALTER TABLE person
+ALTER TABLE person1
 ADD [zone] varchar(10)
---rollback ALTER TABLE person DROP zone state;
+--rollback ALTER TABLE person1 DROP zone state;
 
 --changeset other.dev:6 labels:example-label context:example-context
 --comment: example comment
-ALTER TABLE person
+ALTER TABLE person1
 ADD [field1] varchar(10)
---rollback ALTER TABLE person DROP field1 field1;
+--rollback ALTER TABLE person1 DROP field1 field1;
 
 --changeset other.dev:7 labels:example-label context:example-context
 --comment: example comment
-ALTER TABLE person
+ALTER TABLE person1
 ADD [field2] varchar(100000000000000000000)
---rollback ALTER TABLE person DROP COLUMN field2;
+--rollback ALTER TABLE person1 DROP COLUMN field2;
