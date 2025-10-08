6b586311416296488782c942a2921b2b5286b681
Bikram Kumar <bikram.kumar@harness.io>
2025-10-08 17:23:42 +0530

    migrations

---
+++ db/migrations/V007__bikramkumar.sql
@@ -0,0 +1,7 @@
+CREATE TABLE flywayBikram (
+    id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    address1 VARCHAR(50),
+    address2 VARCHAR(50),
+    city VARCHAR(30)
+);
