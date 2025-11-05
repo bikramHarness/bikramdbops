7ac8d1e9ef22971983e6d31b848ba68acc1002c1
Bikram Kumar <bikram.kumar@harness.io>
2025-11-05 14:32:07 +0530

    migrations

---
+++ db/flyway/mongo/U001_bikramkumar.js
@@ -0,0 +1,21 @@
+
+try {
+
+  if (db.getCollectionNames().includes("users")) {
+    db.users.drop();
+    print("🗑️ Dropped 'users' collection");
+  } else {
+    print("⚠️ 'users' collection does not exist — skipping drop.");
+  }
+
+  if (db.getCollectionNames().includes("orders")) {
+    db.orders.drop();
+    print("🗑️ Dropped 'orders' collection");
+  } else {
+    print("⚠️ 'orders' collection does not exist — skipping drop.");
+  }
+
+} catch (e) {
+  print("Error in U001__drop_init_collections.js:", e);
+  throw e;
+}
