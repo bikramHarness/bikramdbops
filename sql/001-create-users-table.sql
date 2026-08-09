a6da2dce7bdc1d94c34f6712582bb0ba4927550d
Bikram Kumar <bikram.kumar@harness.io>
2026-08-09 16:01:00 +0530

    main

--- sql/001-create-users-table.sql
+++ sql/001-create-users-table.sql
@@ -1,8 +1,9 @@
 --liquibase formatted sql
 
---changeset demo:001
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+--changeset demo:001 runWith:sqlcmd
+--comment: create users table via native sqlcmd executor
+CREATE TABLE users (
+    id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
