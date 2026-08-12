705d6f202ae61c9202e673ffd5b5c147ea0281bb
Bikram Kumar <bikram.kumar@harness.io>
2026-08-12 18:18:40 +0530

    main

--- sql/001-create-users-table.sql
+++ sql/001-create-users-table.sql
@@ -18,3 +18,13 @@ CREATE TABLE users_2 (
     email VARCHAR(255) NOT NULL
 );
 --rollback DROP TABLE users_2;
+
+
+--changeset demo:003 runWith:sqlcmd
+--comment: create users table via native sqlcmd executor
+CREATE TABLE users_3 (
+    id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+--rollback DROP TABLE users_2;
